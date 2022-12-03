d39445d701ac45e8a451464428022d55b8cf0ed6
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-12-03 13:58:18 +0200

    fff

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <DataProvider>
       <NavBar />
       <Headrs/>
-      {/* <BarChart/> */}
+      <BarChart/>
 
       </DataProvider>
    
--- src/components/pages/lineChart/LineChart.jsx
+++ src/components/pages/lineChart/LineChart.jsx
@@ -1,53 +1,53 @@
-import React from 'react'
-import { Line} from 'react-chartjs-2'
+import React, { useEffect, useState } from "react";
+import { Chart as ChartJs, CategoryScale,LinearScale,LineController,LineElement,PointElement
+  ,Title,Tooltip,Legend, BarElement } from "chart.js";
+import { Line} from "react-chartjs-2";
 
-
+ChartJs.register( CategoryScale,LinearScale,LineController,PointElement
+  ,LineElement,Title,Tooltip,Legend, BarElement );
 
 const BarChart = () => {
+ const [chartData,setChartData]=useState({
+  datasets:[]
+ })
+ const [chartOptions,setChartOptions]=useState()
+
+ useEffect(()=>{
+
+  setChartData({
+    labels:["asd","asd","asd","asd","asd"],
+    datasets:[
+      {
+        lable:"woasdasdas",
+        data:[12,33,44,55,66],
+        borderColor:"rgb(43,43,55)",
+        backgroundColor:"rgba(44,566,666,0.4)"
+      }
+    ]
+  })
+  setChartOptions({
+    responsive:true,
+    plugins:{
+      legend:{
+        position:"top"
+      },
+      titel:{
+        display:true,
+        text:"w"
+      }
+    }
+  })
+ },[])
+
   return (
-    <div>
-      <Line
-        data={{
-            
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [
-            {
-              label: '# of votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-            },
-            // {
-            //   label: 'Quantity',
-            //   data: [47, 52, 67, 58, 9, 50],
-            //   backgroundColor: 'orange',
-            //   borderColor: 'red',
-            // },
-          ],
-        }}
-        id={89}
-        height={400}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-        }}
-      />
+    <div className="chartContainer mt-5 text-center">
+Orders in the last 7 days
+<div className="chart p-3">
+      <Line data={chartData} height={40} options={chartOptions}/>
     </div>
-  )
-}
+    </div>
+   
+  );
+};
+
 export default BarChart;
